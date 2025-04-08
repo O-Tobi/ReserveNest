1cc0df62f7aa60632d27c6590adab0015cef566b
unknown <dunjoyebiz2020@gmail.com>
2025-04-08 14:16:47 +0100

    task: done with the body

--- components/ui/Body/Body.tsx
+++ components/ui/Body/Body.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Cuisines from "./Cuisines";
 import FeaturedHotels from "./FeaturedHotels";
 import NearbyHotels from "./NearbyHotels";
 
@@ -8,6 +9,7 @@ export default function Body() {
   return (
     <div className="flex flex-col mx-[30px] mt-[40px] md:mx-[70px] gap-[40px]">
       <FeaturedHotels />
+      <Cuisines />
       <NearbyHotels />
     </div>
   );
