d3031f4a98a3dcfc94c0f01a125f529fcf1b2786
unknown <dunjoyebiz2020@gmail.com>
2025-04-24 14:16:40 +0100

    task: added form validation to the booking page

--- components/ui/HotelPage/BookingForm.tsx
+++ components/ui/HotelPage/BookingForm.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Calendar } from "../calendar";
 import { useEffect, useState } from "react";
 import { format } from "date-fns";
@@ -5,12 +10,26 @@ import { Button } from "../button";
 import { Separator } from "../separator";
 import Image from "next/image";
 import { SquareMinusIcon, SquarePlusIcon } from "lucide-react";
+import { Form, FormLabel, FormField, FormItem, FormControl, FormMessage } from "../form";
+import { Input } from "../input";
 
+// Props type
 type BookingFormProps = {
   foodImg: string;
   foodName: string;
   price: number;
 };
+
+// Schema
+export const bookingSchema = z.object({
+  calendar: z.string().min(1, "Please select a date"),
+  fullName: z.string().min(1, "Full name is required"),
+  email: z.string().email("Enter a valid email address"),
+  phoneNumber: z.string().regex(/^\+?\d{7,14}$/, "Enter a valid phone number"),
+});
+
+// Infer values from schema
+type BookingFormValues = z.infer<typeof bookingSchema>;
 
 export default function BookingForm({
   foodImg,
@@ -21,7 +40,7 @@ export default function BookingForm({
     {
       id: 1,
       mealType: "Breakfast",
-      time: ["7:00 AM", "8:00 AM", "9:00 AM", "7:00 AM", "8:00 AM", "9:00 AM"],
+      time: ["7:00 AM", "8:00 AM", "9:00 AM"],
     },
     {
       id: 2,
@@ -41,20 +60,28 @@ export default function BookingForm({
     time: string[];
   } | null>(null);
 
-  const [datePicked, setDatePicked] = useState<string | undefined>();
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
   const [mealCount, setMealCount] = useState(0);
+  const [timeClicked, setTimeClicked] = useState<string | null>(null);
+  const [timeSlotWarning, setTimeSlotWarning] = useState<string | null>(null);
 
-  const handleMealPlus = () => {
-    setMealCount((prevCount) => prevCount + 1);
-  };
+  const form = useForm<BookingFormValues>({
+    resolver: zodResolver(bookingSchema),
+    defaultValues: {
+      calendar: "",
+      fullName: "",
+      email: "",
+      phoneNumber: "",
+    },
+  });
 
-  const handleMealMinus = () => {
-    if (mealCount !== 0) {
-      setMealCount((prev) => prev - 1);
-    }
-  };
+  //handles meal cart
+  const handleMealPlus = () => setMealCount((prev) => prev + 1);
+  const handleMealMinus = () =>
+    setMealCount((prev) => (prev > 0 ? prev - 1 : 0));
 
+
+  // handles UI change for the option clicked (breakfast, lunch or dinner)
   const timeClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     const clicked = e.currentTarget.value;
     const selectedMeal = mealTimes.find((meal) => meal.mealType === clicked);
@@ -64,132 +91,233 @@ export default function BookingForm({
     }
   };
 
+  const timeSlotHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const clicked = e.currentTarget.value;
+    setTimeClicked(clicked); //saves users choice
+    setTimeSlotWarning(null);
+  };
+
+  // handles the preselected instance of the meal 
   useEffect(() => {
-    const preDisplayedTime = mealTimes.find((m) => m.mealType === "Breakfast");
-    if (preDisplayedTime) {
-      setMealSelect(preDisplayedTime);
-      setSelectedTime("Breakfast");
+    const preSelected = mealTimes.find((m) => m.mealType === "Breakfast");
+    if (preSelected) {
+      setMealSelect(preSelected);
+      setSelectedTime("Breakfast"); 
     }
   }, []);
 
+  const totalCost = price * mealCount;
+
+  const onSubmit = (data: BookingFormValues) => {
+    if(!timeClicked) {
+      setTimeSlotWarning("Please select a time")
+      return; //check time validity later such that you wonrt be able to apply for time has passed
+    }
+    console.log({
+      ...data,
+      selectedMeal: mealSelect?.mealType,
+      timeClicked,
+      mealCount,
+      totalCost,
+
+      // clear the form after submission
+    });
+    setTimeSlotWarning(null);
+  };
+
   return (
-    <div className="flex flex-col md:flex-row lg:flex-col w-full gap-[24px]">
-      {/* calendar here */}
-      <div className="calendar flex flex-col gap-[16px]">
-        <h2 className="text-[22px] text-[darkGreen]">Choose Date</h2>
-        <Calendar
-          mode="single"
-          selected={datePicked ? new Date(datePicked) : undefined}
-          onSelect={(date) => {
-            if (date) {
-              const formatted = format(date, "yyyy-MM-dd");
-              setDatePicked(formatted);
-            }
-          }}
-          modifiersStyles={{
-            selected: {
-              backgroundColor: "#004225",
-              color: "white",
-            },
-          }}
-          className="bg-white flex justify-center text-[darkGreen] rounded-[16px] "
-        />
-      </div>
-
-      {/* time slots here */}
-      <div className="timeSlot flex flex-col gap-[16px]">
-        <h2 className="text-[22px] text-[darkGreen]">Choose time slot</h2>
-
-        <div className="flex flex-col w-full bg-white rounded-[16px] p-[20px] gap-4">
-          {/* Row for meal types */}
-          <div className="flex gap-0 justify-between">
-            {mealTimes.map((meal, id) => (
-              <div className="flex flex-col w-1/3" key={id}>
-                <Button
-                  variant="ghost"
-                  className="flex justify-center  text-[darkGreen] text-[12px]"
-                  onClick={timeClickHandler}
-                  value={meal.mealType}
-                >
-                  {meal.mealType}
-                </Button>
-
-                <Separator
-                  className={`border-[2px] w-full border-[darkGreen]/40 ${
-                    selectedTime === meal.mealType
-                      ? "border-[darkGreen]"
-                      : "border-[darkGreen]/40"
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
-
-          {/* Row for times under selected meal */}
-          {mealSelect && (
-            <div className="flex justify-start items-center gap-[12px] flex-wrap">
-              {mealSelect.time.map((t, index) => (
-                <Button
-                  value={t}
-                  onClick={() => console.log(t)}
-                  variant="outline"
-                  key={index}
-                  className="border-[darkGreen]  text-[12px] text-[darkGreen] p-[12px] gap-[10px] rounded-md text-sm"
-                >
-                  {t}
-                </Button>
-              ))}
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col md:flex-row lg:flex-col w-full gap-[24px]"
+      >
+        {/* Calendar */}
+        <FormField
+          control={form.control}
+          name="calendar"
+          render={({ field }) => (
+            <div className="calendar flex flex-col gap-[16px]">
+              <h2 className="text-[22px] text-[darkGreen]">Choose Date</h2>
+              <Calendar
+                mode="single"
+                selected={field.value ? new Date(field.value) : undefined}
+                onSelect={(date) => {
+                  if (date) {
+                    const formatted = format(date, "yyyy-MM-dd");
+                    field.onChange(formatted);
+                  }
+                }}
+                modifiersStyles={{
+                  selected: {
+                    backgroundColor: "#004225",
+                    color: "white",
+                  },
+                }}
+                className="bg-white flex justify-center text-[darkGreen] rounded-[16px]"
+              />
             </div>
           )}
+          
+        />
+
+        {/* Time Slots */}
+        <div className="timeSlot flex flex-col gap-[16px]">
+          <h2 className="text-[22px] text-[darkGreen]">Choose time slot</h2>
+          <div className="flex flex-col w-full bg-white rounded-[16px] p-[20px] gap-4">
+            <div className="flex gap-0 justify-between">
+              {mealTimes.map((meal) => (
+                <div className="flex flex-col w-1/3" key={meal.id}>
+                  <Button
+                    variant="ghost"
+                    className="flex justify-center text-[darkGreen] text-[12px]"
+                    onClick={timeClickHandler}
+                    value={meal.mealType}
+                    type="button"
+                  >
+                    {meal.mealType}
+                  </Button>
+                  <Separator
+                    className={`border-[2px] w-full transition-colors duration-500 ${
+                      selectedTime === meal.mealType
+                        ? "border-[darkGreen]"
+                        : "border-[darkGreen]/40"
+                    }`}
+                  />
+                </div>
+              ))}
+            </div>
+
+            {mealSelect && (
+              <div className="flex justify-start items-center gap-[12px] flex-wrap">
+                {mealSelect.time.map((t, index) => (
+                  <Button
+                    key={index}
+                    value={t}
+                    onClick={timeSlotHandler}
+                    variant="outline"
+                    className={`border-[darkGreen] text-[12px] text-[darkGreen] p-[12px] gap-[10px] rounded-md ${
+                      timeClicked === t ? "bg-[darkGreen] text-white" : "bg-none"
+                    }`}
+                    type="button"
+                  >
+                    {t}
+                  </Button>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+       {timeSlotWarning && ( <p className="text-red-500">{timeSlotWarning}</p>)}
 
-      {/* FoodChosen here */}
-      {/* map through the list from the context */}
-      <div className=" flex flex-col gap-[16px]">
-        <h2 className="text-[22px] text-[darkGreen]">Food Chosen</h2>
-
-        <div className="foods flex flex-col bg-white rounded-[16px] p-[20px] gap-[32px]">
-          {/* individual food */}
-          <div className="food flex  gap-[16px] ">
-            <Image
-              src={foodImg}
-              alt={foodName}
-              width={76}
-              height={76}
-              className="object-cover rounded-[12px]"
-            />
-
-            <div className="foodDetails flex flex-col w-full gap-[8px]">
-              <h3 className="text-[16px] text-[darkGreen]">{foodName}</h3>
-              <p className="text-[12px] text-[darkGreen]">Vegetarian</p>
-              <div className="flex justify-between items-center py-[4px]">
-                <p className="text-[16px] tracking-[0.5px] font-medium text-[lightGreen]">
-                  ${price}
-                </p>
-                <div className="flex  items-center ">
-                  <Button onClick={handleMealMinus}>
-                    {" "}
-                    <SquareMinusIcon />{" "}
-                  </Button>
-                  {mealCount}
-                  <Button onClick={handleMealPlus}>
-                    {" "}
-                    <SquarePlusIcon />{" "}
-                  </Button>
+        {/* Food Chosen */}
+        <div className="flex flex-col gap-[16px]">
+          <h2 className="text-[22px] text-[darkGreen]">Food Chosen</h2>
+          <div className="foods flex flex-col bg-white rounded-[16px] p-[20px] gap-[32px]">
+            <div className="food flex gap-[16px]">
+              <Image
+                src={foodImg}
+                alt={foodName}
+                width={76}
+                height={76}
+                className="object-cover rounded-[12px]"
+              />
+              <div className="foodDetails flex flex-col w-full gap-[8px]">
+                <h3 className="text-[16px] text-[darkGreen]">{foodName}</h3>
+                <p className="text-[12px] text-[darkGreen]">Vegetarian</p>
+                <div className="flex justify-between items-center py-[4px]">
+                  <p className="text-[16px] tracking-[0.5px] font-medium text-[lightGreen]">
+                    ${price}
+                  </p>
+                  <div className="flex items-center gap-2">
+                    <Button onClick={handleMealMinus} type="button">
+                      <SquareMinusIcon />
+                    </Button>
+                    {mealCount}
+                    <Button onClick={handleMealPlus} type="button">
+                      <SquarePlusIcon />
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <Separator className="border-[0.5px] border-[#C5C5C5]/50 " />
-
-          <div className="totalCost flex items-center justify-between text-[22px] text-[darkGreen] ">
-            <p>Total Cost:</p>
-            <p>$TBD</p>
+            <Separator className="border-[0.5px] border-[#C5C5C5]/50" />
+            <div className="totalCost flex items-center justify-between text-[22px] text-[darkGreen]">
+              <p>Total Cost:</p>
+              <p>${totalCost}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+
+        {/* Booking Details */}
+        <div className="bookingDetails flex flex-col gap-[16px]">
+          <h2 className="text-[22px] text-[darkGreen]">Booking details</h2>
+          <div className="details flex flex-col bg-white rounded-[16px] p-[20px] gap-[20px]">
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-[darkGreen]">Full Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Enter your full name"
+                      className="border-[darkGreen]/50 text-[darkGreen] rounded-[8px]"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500"/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-[darkGreen]">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="example@mail.com"
+                      className="border-[darkGreen]/50 text-[darkGreen] rounded-[8px]"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500"/>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-[darkGreen]">
+                    Phone Number
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="tel"
+                      placeholder="+234 000 000 0000"
+                      className="border-[darkGreen]/50 text-[darkGreen] rounded-[8px]"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500"/>
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+
+        {/* Submit */}
+        <Button type="submit" className="self-start bg-[darkGreen] text-white">
+          Book Now
+        </Button>
+      </form>
+    </Form>
   );
 }
