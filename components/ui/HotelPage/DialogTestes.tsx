6a8028650350e70d8b5c40aca88e813c0636cb89
unknown <dunjoyebiz2020@gmail.com>
2025-05-01 22:26:44 +0100

    task: implemented the booking confirmation for desktop

--- components/ui/HotelPage/DialogTestes.tsx
+++ components/ui/HotelPage/DialogTestes.tsx
@@ -18,7 +18,14 @@ import {
   DialogDescription,
   DialogHeader,
 } from "../dialog";
-import { Check } from "lucide-react";
+import {
+  CalendarDays,
+  Check,
+  CircleX,
+  ClipboardList,
+  Clock4,
+  UsersRound,
+} from "lucide-react";
 import { Separator } from "../separator";
 
 export default function DialogTestes() {
@@ -30,8 +37,8 @@ export default function DialogTestes() {
         <DialogTrigger asChild>
           <Button variant="outline">Open</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-3/6 bg-white border-none  justify-center items-center p-[32px] gap-[28px]">
-          <DialogHeader className="flex w-full justify-center items-center  gap-[20px] bg-red-500">
+        <DialogContent className="sm:max-w-3/6 bg-white border-none  justify-center items-center p-[32px] gap-[24px]">
+          <DialogHeader className="flex w-full justify-center items-center  gap-[20px]">
             <div className="checkmark flex justify-center items-center bg-[#23A26D]/12 rounded-full w-[120px] h-[120px]">
               <div className="inner flex items-center justify-center h-[66px] w-[66px] rounded-full bg-[#23A26D] text-white">
                 <Check className="h-10 w-10" />
@@ -46,8 +53,44 @@ export default function DialogTestes() {
           <Separator className="border-[.4px] border-[#C5C5C5]/40 w-full" />
 
           <DialogDescription>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi deleniti, voluptate voluptatem laudantium veritatis debitis nesciunt quo maxime, magnam odio praesentium ipsa fuga vel, commodi reprehenderit quia quam aliquam!</p>
+            <div className="flex flex-col justify-center items-center gap-[14px]">
+              <p className="hotelName text-[28px] font-medium">
+                Colours by Royal Cafe
+              </p>
+              <p className="address">
+                3rd Floor, Royal Inn, 9/7, Opposite SaharaGanj Mall, Shahnajaf
+                Road, Hazratganj, Lucknow
+              </p>
+            </div>
+          </DialogDescription>
+
+          <DialogDescription>
+            <div className="infoTab flex flex-row gap-[24px] justify-between items-center">
+              <div className="flex w-full flex-col justify center items-center gap-[16px] px-[16px] py-[20px] rounded-[8px] border-[0.5px] border-[#C5C5C5]/40">
+                <CalendarDays />
+                <p>19 Jan 2025</p>
+              </div>
+
+              <div className="flex w-full flex-col justify center items-center gap-[16px] px-[16px] py-[20px] rounded-[8px] border-[0.5px] border-[#C5C5C5]/40">
+                <Clock4 />
+                <p>9:00 PM</p>
+              </div>
+
+              <div className="flex w-full flex-col justify center items-center gap-[16px] px-[16px] py-[20px] rounded-[8px] border-[0.5px] border-[#C5C5C5]/40">
+                <UsersRound />
+                <p>2 Guests</p>
+              </div>
+            </div>
+          </DialogDescription>
+
+          <DialogDescription>
+            <div className="buttons  w-full flex gap-[24px] justify-between items-center">
+              <Button className="bg-[#006FD5] p-[12px] gap-[10px] h-[54px] w-1/2  text-white text-[14px] font-medium rounded-[8px]">
+                <ClipboardList /> View Menu
+              </Button>
+              <Button className="bg-[#BA1717] p-[12px] gap-[10px] h-[54px] w-1/2 text-white text-[14px] font-medium rounded-[8px]">
+                <CircleX /> Cancel Booking
+              </Button>
             </div>
           </DialogDescription>
         </DialogContent>
